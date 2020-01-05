a3c6eb003bf55456631c3beaae9bbbc4723969c2
bhavyajeet <bhavyajeet29@gmail.com>
2020-01-06 02:51:20 +0530

    hum node

--- humidifier_code/humidifier_code.ino
+++ humidifier_code/humidifier_code.ino
@@ -1,12 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
  
-const char* ssid = "motog";
+const char* ssid = "moto g";
 const char* password = "justmonika";
  
 void setup () {
  
 Serial.begin(115200);
+Serial.print(password);
 WiFi.begin(ssid, password);
  
 while (WiFi.status() != WL_CONNECTED) {
@@ -24,7 +25,7 @@ if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
  
 HTTPClient http;  //Declare an object of class HTTPClient
  
-http.begin("http://jsonplaceholder.typicode.com/users/1");  //Specify request destination
+http.begin("http://192.168.43.9:8080/~/in-cse/in-name/Project_1_Indoor_Air_Pollution_Management/Classroom_1/la");  //Specify request destination
 http.addHeader("Content-Type", "application/json");
     http.addHeader("X-M2M-Origin", "admin:admin");
 int httpCode = http.GET();                                                        
